d90837bd140fba3b8982d65b055abca4a42f7cb1
Jez <jezdavisuk@gmail.com>
2024-09-18 11:27:43 +0100

    feat: add GSON deserialisation with explicit mapping from JSON key names to instance variables on class Album

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Album.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Album.java
@@ -1,13 +1,28 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Album {
 
+    @SerializedName("id")
     Long id;
+
+    @SerializedName("recordName")
     String recordName;
+
+    @SerializedName("artist")
     String artist;
+
+    @SerializedName("yearOfRelease")
     int yearOfRelease;
+
+    @SerializedName("genre")
     Genre genre;
+
+    @SerializedName("quantityInStock")
     int quantityInStock;
+
+    @SerializedName("available")
     boolean available;
 
     public Album() {
