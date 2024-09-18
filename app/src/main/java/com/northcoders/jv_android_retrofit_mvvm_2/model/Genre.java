80412aa6eca92b9119b83dfc2f1be158ad07bb2f
Jez <jezdavisuk@gmail.com>
2024-09-18 11:17:34 +0100

    feat: add and populate enum class Genre on package model

---
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Genre.java
@@ -0,0 +1,23 @@
+package com.northcoders.jv_android_retrofit_mvvm_2.model;
+
+public enum Genre {
+    ALTERNATIVE,
+    BLUES,
+    CLASSIC,
+    COUNTRY,
+    DRUM_AND_BASS,
+    HIP_HOP,
+    HOUSE,
+    INDIE,
+    JAZZ,
+    METAL,
+    POP,
+    PUNK,
+    RAP,
+    REGGAE,
+    RHYTHM_AND_BLUES,
+    SOUL,
+    TECHNO,
+    TRANCE,
+    WORLD
+}
