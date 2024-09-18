6c9cbc3804521ab13f28d768f9b00e5ce1a2b163
Jez <jezdavisuk@gmail.com>
2024-09-18 12:16:30 +0100

    feat: add method getMutableLiveData to class AlbumRepository transforms service layer data to observable LiveData format

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/AlbumRepository.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/AlbumRepository.java
@@ -1,10 +1,19 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.model;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.northcoders.jv_android_retrofit_mvvm_2.service.AlbumApiService;
+import com.northcoders.jv_android_retrofit_mvvm_2.service.RetrofitInstance;
+
 import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AlbumRepository {
 
@@ -13,5 +22,29 @@ public class AlbumRepository {
 
     public AlbumRepository(Application application) {
         this.application = application;
+    }
+
+    public MutableLiveData<List<Album>> getMutableLiveData() {
+
+        AlbumApiService albumApiService = RetrofitInstance.getService();
+        Call<List<Album>> call = albumApiService.getAllAlbums();
+
+        call.enqueue(new Callback<List<Album>>() {
+
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                List<Album> albums = response.body();
+                mutableLiveData.setValue(albums);
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable t) {
+                Log.e("HTTP Failure", Objects.requireNonNull(t.getMessage()));
+            }
+
+        });
+
+        return mutableLiveData;
+
     }
 }
