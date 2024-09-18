c75c2b0d9776c2e93c0ff478fb88a492295bb106
Jez <jezdavisuk@gmail.com>
2024-09-18 11:43:45 +0100

    feat: add method getService() to class RetrofitInstance builds instance of Retrofit

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/service/RetrofitInstance.java
@@ -1,10 +1,22 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.service;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
 
     private static Retrofit retrofit = null;
 
     private final static String BASE_URL = "http://10.0.2.2:8080/api/v1/";
+
+    public static AlbumApiService getService() {
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+    }
+
 }
