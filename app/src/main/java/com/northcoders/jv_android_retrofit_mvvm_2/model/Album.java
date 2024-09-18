de086166bc92f8a672979ecf7555134c63556b5c
Jez <jezdavisuk@gmail.com>
2024-09-18 11:16:50 +0100

    feat: add JSON-appropriate fields to class Album

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Album.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Album.java
@@ -1,4 +1,13 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.model;
 
 public class Album {
+
+    Long id;
+    String recordName;
+    String artist;
+    int yearOfRelease;
+    Genre genre;
+    int quantityInStock;
+    boolean available;
+
 }
