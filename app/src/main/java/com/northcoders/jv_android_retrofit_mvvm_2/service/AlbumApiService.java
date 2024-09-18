7dbe34b77ea2e23807d32deecc86eebf1954379d
Jez <jezdavisuk@gmail.com>
2024-09-18 11:47:48 +0100

    feat: add abstract method getAllAlbums() to interface AlbumApiService returns List of type Album

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/service/AlbumApiService.java
@@ -1,4 +1,14 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.service;
 
+import com.northcoders.jv_android_retrofit_mvvm_2.model.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface AlbumApiService {
+
+    @GET
+    Call<List<Album>> getAllAlbums();
 }
