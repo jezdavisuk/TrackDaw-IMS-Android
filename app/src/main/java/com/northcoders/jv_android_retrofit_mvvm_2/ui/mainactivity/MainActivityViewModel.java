3b6e053445251b7f70810d031f88dac9bc727b24
Jez <jezdavisuk@gmail.com>
2024-09-18 12:28:54 +0100

    feat: add method getAllAlbums() to class MainActivityViewModel retrieves MutableLiveData on instance of class AlbumRepository

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/MainActivityViewModel.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/MainActivityViewModel.java
@@ -4,8 +4,12 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
+import com.northcoders.jv_android_retrofit_mvvm_2.model.Album;
 import com.northcoders.jv_android_retrofit_mvvm_2.model.AlbumRepository;
+
+import java.util.List;
 
 public class MainActivityViewModel extends AndroidViewModel {
 
@@ -14,5 +18,9 @@ public class MainActivityViewModel extends AndroidViewModel {
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
         this.albumRepository = new AlbumRepository();
+    }
+
+    public LiveData<List<Album>> getAllAlbums() {
+        return albumRepository.getMutableLiveData();
     }
 }
