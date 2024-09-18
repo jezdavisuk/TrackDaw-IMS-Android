233e693abd4c483abd9f3b3e5bbee0f7cdc1331e
Jez <jezdavisuk@gmail.com>
2024-09-18 12:04:49 +0100

    feat: attach MutableLiveData field and application context field to class AlbumRepository

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/AlbumRepository.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/AlbumRepository.java
@@ -1,4 +1,13 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.model;
 
+import android.app.Application;
+
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
 public class AlbumRepository {
+
+    private MutableLiveData<List<Album>> mutableLiveData = new MutableLiveData<>();
+    private Application application = new Application();
 }
