9bdb1f770c9903712c0f11f646634469df13da63
Jez <jezdavisuk@gmail.com>
2024-09-18 11:18:52 +0100

    feat: add all args and no args constructor to class Album

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Album.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Album.java
@@ -10,4 +10,16 @@ public class Album {
     int quantityInStock;
     boolean available;
 
+    public Album() {
+    }
+
+    public Album(Long id, String recordName, String artist, int yearOfRelease, Genre genre, int quantityInStock, boolean available) {
+        this.id = id;
+        this.recordName = recordName;
+        this.artist = artist;
+        this.yearOfRelease = yearOfRelease;
+        this.genre = genre;
+        this.quantityInStock = quantityInStock;
+        this.available = available;
+    }
 }
