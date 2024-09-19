cb9d3480b47bd13c899730ccda24caea268ed5cb
Jez <jezdavisuk@gmail.com>
2024-09-19 12:09:04 +0100

    feat: add custom adapter class AlbumAdapter extends RecyclerView.Adapter<>

---
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/AlbumAdapter.java
@@ -0,0 +1,6 @@
+package com.northcoders.jv_android_retrofit_mvvm_2.ui.mainactivity;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AlbumAdapter extends RecyclerView.Adapter<> {
+}
