fc72a97047faf654c4499fde6899fe725cdc2d6b
Jez <jezdavisuk@gmail.com>
2024-09-19 16:12:30 +0100

    feat: add fields to class AddAlbumClickHandlers

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/addAlbum/AddAlbumClickHandlers.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/addAlbum/AddAlbumClickHandlers.java
@@ -1,4 +1,15 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.ui.addAlbum;
 
+import android.content.Context;
+
+import com.northcoders.jv_android_retrofit_mvvm_2.model.Album;
+import com.northcoders.jv_android_retrofit_mvvm_2.ui.mainactivity.MainActivityViewModel;
+
 public class AddAlbumClickHandlers {
+
+    private Album album;
+    private Context context;
+    private MainActivityViewModel viewModel;
+
+
 }
