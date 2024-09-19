47bb7ad5b0d40a8a4844b1a91a5062d000c59c07
Jez <jezdavisuk@gmail.com>
2024-09-19 15:13:28 +0100

    feat: initialise instance variables ActivityMainBinding and MainActivityViewModel with body of method onCreate() of class MainActivity

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.northcoders.jv_android_retrofit_mvvm_2.R;
@@ -25,13 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_main
+        );
+
+        viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+
+
+        getAllAlbums();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void getAllAlbums() {
+
     }
 }
