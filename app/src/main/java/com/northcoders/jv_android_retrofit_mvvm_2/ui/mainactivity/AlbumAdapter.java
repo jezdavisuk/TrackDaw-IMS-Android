1062e7cf295605dd2879f5e28059babb8dd08f9f
Jez <jezdavisuk@gmail.com>
2024-09-19 12:13:01 +0100

    feat: to adapter class AlbumAdapte  add inner class AlbumViewHolder extends RecyclerView.ViewHolder

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/AlbumAdapter.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/AlbumAdapter.java
@@ -3,4 +3,8 @@ package com.northcoders.jv_android_retrofit_mvvm_2.ui.mainactivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class AlbumAdapter extends RecyclerView.Adapter<> {
+
+    public static class AlbumViewHolder extends RecyclerView.ViewHolder {
+
+    }
 }
