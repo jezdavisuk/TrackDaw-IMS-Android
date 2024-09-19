e98e9906f24fc8ded2b6cd42c88ec769a75881b5
Jez <jezdavisuk@gmail.com>
2024-09-19 11:11:04 +0100

    add POST method addAlbum to interface AlbumApiService

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/service/AlbumApiService.java
@@ -5,10 +5,14 @@ import com.northcoders.jv_android_retrofit_mvvm_2.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 
 public interface AlbumApiService {
 
-    @GET
+    @GET("album")
     Call<List<Album>> getAllAlbums();
+
+    @GET("album")
+    Call<Album> addAlbum(@Body Album album);
 }
