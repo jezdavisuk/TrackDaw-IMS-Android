2968441dfc9ffafcaf1f3b9162eeece4c7561130
Jez <jezdavisuk@gmail.com>
2024-09-19 12:16:06 +0100

    feat: add constructor to class AlbumAdapter.AlbumViewHolder sets new field of type AlbumItemBinding

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/AlbumAdapter.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/AlbumAdapter.java
@@ -2,9 +2,18 @@ package com.northcoders.jv_android_retrofit_mvvm_2.ui.mainactivity;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.northcoders.jv_android_retrofit_mvvm_2.databinding.AlbumItemBinding;
+
 public class AlbumAdapter extends RecyclerView.Adapter<> {
 
     public static class AlbumViewHolder extends RecyclerView.ViewHolder {
+
+        private AlbumItemBinding albumItemBinding;
+
+        public AlbumViewHolder(AlbumItemBinding albumItemBinding) {
+            super(albumItemBinding.getRoot());
+            this.albumItemBinding = albumItemBinding;
+        }
 
     }
 }
