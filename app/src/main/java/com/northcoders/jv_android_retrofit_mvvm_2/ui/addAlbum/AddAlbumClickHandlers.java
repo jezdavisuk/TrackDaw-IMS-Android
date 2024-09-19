d44e555d81551a94676fc06cd33f0321e3ae9d3c
Jez <jezdavisuk@gmail.com>
2024-09-19 16:33:57 +0100

    feat: add method onSubmitButtonClicked() on class AddAlbumClickHandlers handles buttonAddAlbum

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/addAlbum/AddAlbumClickHandlers.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/addAlbum/AddAlbumClickHandlers.java
@@ -1,8 +1,12 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.ui.addAlbum;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 
 import com.northcoders.jv_android_retrofit_mvvm_2.model.Album;
+import com.northcoders.jv_android_retrofit_mvvm_2.ui.mainactivity.MainActivity;
 import com.northcoders.jv_android_retrofit_mvvm_2.ui.mainactivity.MainActivityViewModel;
 
 public class AddAlbumClickHandlers {
@@ -16,4 +20,33 @@ public class AddAlbumClickHandlers {
         this.context = context;
         this.viewModel = viewModel;
     }
+
+    public void onSubmitButtonClicked(View view) {
+
+        if (album.getRecordName() == null || album.getArtist() == null || album.getYearOfRelease() == 0 || album.getGenre() == null || album.getQuantityInStock() == 0) {
+
+            Toast.makeText(context, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Intent intent = new Intent(context, MainActivity.class);
+
+            Album newAlbum = new Album(
+                    album.getId(),
+                    album.getRecordName(),
+                    album.getArtist(),
+                    album.getYearOfRelease(),
+                    album.getGenre(),
+                    album.getQuantityInStock(),
+                    album.isAvailable()
+            );
+
+
+            viewModel.addNewAlbum(newAlbum);
+
+            context.startActivity(intent);
+        }
+
+    }
+
 }
