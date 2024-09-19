ae9c6e87127c39d331e35aea16b21b6996e51482
Jez <jezdavisuk@gmail.com>
2024-09-19 11:53:50 +0100

    feat: enable data binding of fields on class Album bindable to and from UI

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Album.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/model/Album.java
@@ -1,8 +1,12 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.model;
 
-import com.google.gson.annotations.SerializedName;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
 
-public class Album {
+import com.google.gson.annotations.SerializedName;
+import com.northcoders.jv_android_retrofit_mvvm_2.BR;
+
+public class Album extends BaseObservable {
 
     @SerializedName("id")
     Long id;
@@ -35,6 +39,74 @@ public class Album {
         this.yearOfRelease = yearOfRelease;
         this.genre = genre;
         this.quantityInStock = quantityInStock;
+        this.available = available;
+    }
+
+    @Bindable
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+        notifyPropertyChanged(BR.id);
+    }
+
+    @Bindable
+    public String getRecordName() {
+        return recordName;
+    }
+
+    public void setRecordName(String recordName) {
+        this.recordName = recordName;
+        notifyPropertyChanged(BR.recordName);
+    }
+
+    @Bindable
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+        notifyPropertyChanged(BR.artist);
+    }
+
+    @Bindable
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+        notifyPropertyChanged(BR.yearOfRelease);
+    }
+
+    @Bindable
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+        notifyPropertyChanged(BR.genre);
+    }
+
+    @Bindable
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+        notifyPropertyChanged(BR.quantityInStock);
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
         this.available = available;
     }
 }
