4e495b80e12e578b00304603968b6ab1baf19014
Jez <jezdavisuk@gmail.com>
2024-09-23 12:27:44 +0100

    feat: add constructor to class MainActivityClickHandler assigns instance variable of type Context

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/MainActivityClickHandler.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_2/ui/mainactivity/MainActivityClickHandler.java
@@ -1,4 +1,12 @@
 package com.northcoders.jv_android_retrofit_mvvm_2.ui.mainactivity;
 
+import android.content.Context;
+
 public class MainActivityClickHandler {
+
+    Context context;
+
+    public MainActivityClickHandler(Context context) {
+        this.context = context;
+    }
 }
